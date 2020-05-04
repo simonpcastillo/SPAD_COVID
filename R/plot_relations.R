770931dd8e5ec94eea115ff472a1b744e71e730a
simonpcastillo <spcastil@uc.cl>
2020-05-04 16:53:31 -0400

    Update pkg and plots

--- R/plot_relations.R
+++ R/plot_relations.R
@@ -14,7 +14,8 @@ plot_relations <- function(CPAD, saveplot=TRUE, saveplot.ext= ".png"){
   plotrelations <- list()
   df4 = CPAD
   breaksdate = seq(range(CPAD$day)[1],range(CPAD$day)[2], length.out = 5) #c(min(df2b$date),as.Date("2020-02-28"),as.Date("2020-02-29"),as.Date("2020-04-14"),max(df2b$date))#
-  FF = ggplot(df4, aes(log(alfa), (NcasesPos),colour=as.numeric(day))) +
+
+   FF = ggplot(df4, aes(log(alfa), (NcasesPos),colour=as.numeric(day))) +
     geom_point(aes(group = seq_along(day)), size=2)+
     #geom_path(size=1.5, alpha=0.3)+
     labs(x= expression(paste("ln(",alpha, ")")), y= expression("#SARS-CoV-2(+)"), title = "A. ") +
@@ -29,9 +30,9 @@ plot_relations <- function(CPAD, saveplot=TRUE, saveplot.ext= ".png"){
   G = ggplot(df4, aes(log(beta),countries,colour=as.numeric(day))) +
     geom_point(aes(group = seq_along(day)), size=2)+
     #geom_path(size=1.5, alpha=0.3)+
-    labs(x= expression(paste("ln(",beta, ")")), y= expression("# Countries"), title= "B. ") +
+    labs(x= expression(paste("ln(",beta, ")")), y= expression("# Countries"), title= "D. ") +
     theme_minimal() +
-    scale_y_continuous(limits = c(-0.1,170), expand  = c(-0.1, NA))+
+    #scale_y_continuous(limits = c(-0.1,170), expand  = c(-0.1, NA))+
     #guides(colour=FALSE)+
     theme(axis.title=element_text(size=12,face="bold"))+
     scale_colour_viridis_c(breaks = as.numeric(breaksdate),
@@ -43,7 +44,7 @@ plot_relations <- function(CPAD, saveplot=TRUE, saveplot.ext= ".png"){
 
   FFinset = ggplot(df4, aes((alfa), NcasesPos,colour=as.numeric(day))) +
     geom_path(size=1, colour="gray60")+
-    labs(x= expression(paste(alpha)), y= expression(paste("# SARS-Cov-2"))) +
+    labs(x= expression(paste(alpha)), y= expression(paste(""))) +
     theme_minimal() +
     theme(rect = element_rect(fill = "transparent"),
           axis.text.y = element_blank(),
@@ -58,7 +59,7 @@ plot_relations <- function(CPAD, saveplot=TRUE, saveplot.ext= ".png"){
 
   Ginset=ggplot(df4, aes((beta),countries,colour=as.numeric(day))) +
     geom_path(size=1, colour="gray60")+
-    labs(x= expression(paste(beta)), y= expression(paste("# Countries"))) +
+    labs(x= expression(paste(beta)), y= expression(paste(""))) +
     theme_minimal() +
     scale_x_continuous(breaks = c(0, 3000000, 6000000))+
     theme(rect = element_rect(fill = "transparent"),
@@ -74,14 +75,81 @@ plot_relations <- function(CPAD, saveplot=TRUE, saveplot.ext= ".png"){
                              ymin = 90, ymax=175, xmin= sum(range(log(df4$beta)))/2, xmax=Inf)
 
 
-  plotrelations <<- list.append(plotrelations, alphaSARS= FF2, betaCountries=G2)
 
-  print(FF2|G2)
+  ###############
+
+  HH = ggplot(df4, aes(log(alfa), (countries),colour=as.numeric(day))) +
+    geom_point(aes(group = seq_along(day)), size=2)+
+    #geom_path(size=1.5, alpha=0.3)+
+    labs(x= expression(paste("ln(",alpha, ")")), y= expression("#Countries"), title = "C. ") +
+    theme_minimal() +
+    #scale_y_continuous(limits = c(-10000,3000000), expand  = c(-10000, 3000000))+
+    guides(colour=FALSE)+
+    theme(axis.title=element_text(size=12,face="bold"))+
+    scale_colour_viridis_c(breaks = as.numeric(breaksdate),
+                           labels = paste0(day(breaksdate), "-", month(breaksdate, label = TRUE)),
+                           name = "Day",
+                           option = "plasma")
+
+  I = ggplot(df4, aes(log(beta),NcasesPos,colour=as.numeric(day))) +
+    geom_point(aes(group = seq_along(day)), size=2)+
+    #geom_path(size=1.5, alpha=0.3)+
+    labs(x= expression(paste("ln(",beta, ")")), y= expression("#SARS-CoV-2(+) "), title= "B. ") +
+    theme_minimal() +
+    #scale_y_continuous(limits = c(-0.1,170), expand  = c(-0.1, NA))+
+    guides(colour=FALSE)+
+    theme(axis.title=element_text(size=12,face="bold"))+
+    scale_colour_viridis_c(breaks = as.numeric(breaksdate),
+                           labels = paste0(day(breaksdate), "-", month(breaksdate, label = TRUE)),
+                           name = "Day",
+                           option = "plasma")
+
+
+
+  HHinset = ggplot(df4, aes((alfa), countries,colour=as.numeric(day))) +
+    geom_path(size=1, colour="gray60")+
+    labs(x= expression(paste(alpha)), y= expression(paste(""))) +
+    theme_minimal() +
+    theme(rect = element_rect(fill = "transparent"),
+          axis.text.y = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())+
+    guides(colour=FALSE)+
+    theme(axis.title=element_text(size=12,face="bold"))
+
+
+  HH2 = HH + annotation_custom(grob=ggplotGrob(HHinset),
+                               ymin = 90, ymax=170, xmin= sum(range(log(df4$alfa)))/2, xmax=Inf)
+
+  Iinset=ggplot(df4, aes((beta),NcasesPos,colour=as.numeric(day))) +
+    geom_path(size=1, colour="gray60")+
+    labs(x= expression(paste(beta)), y= expression(paste(""))) +
+    theme_minimal() +
+    scale_x_continuous(breaks = c(0, 3000000, 6000000))+
+    theme(rect = element_rect(fill = "transparent"),
+          axis.text.y = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())+
+    #scale_x_log10(labels=trans_format('log10',math_format(10^.x)))+
+    guides(colour=FALSE)+
+    theme(axis.title=element_text(size=12,face="bold"))
+
+
+  I2 = I + annotation_custom(grob=ggplotGrob(Iinset),
+                             ymin = 1000000, ymax=2500000, xmin= sum(range(log(df4$beta)))/2, xmax=Inf)
+
+
+  plotrelations <<- list.append(plotrelations, alphaSARS= FF2,
+                                betaCountries=G2,
+                                alphaCountries = HH2,
+                                betaSARS = I2)
+
+  print((FF2/HH2)|(I2/G2))
 
   if(saveplot==TRUE){
     dir.create("plots")
     dir.create("plots/betaplots")
-    ggsave(plot = FF2|G2, filename = paste("plots/betaplots/relations", saveplot.ext), width = 25, height = 12, units = "cm")
+    ggsave(plot = ((FF2/HH2)|(I2/G2)), filename = paste("plots/betaplots/relations", saveplot.ext), width = 25, height = 12, units = "cm")
   }
 
 }#ElFin
